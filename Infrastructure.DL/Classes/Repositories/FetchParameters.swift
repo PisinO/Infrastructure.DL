3e95b0e4adb140d35bc96ecff2c00d60b2801096
Ondřej Pišín <ondrej.pisin@gmail.com>
2020-09-14 19:34:24 +0200

    Made FetchParameters public

--- Infrastructure.DL/Classes/Repositories/FetchParameters.swift
+++ Infrastructure.DL/Classes/Repositories/FetchParameters.swift
@@ -9,10 +9,17 @@
 import Foundation
 
 public struct FetchParameters {
-    public let predicate: NSPredicate?
-    public let sortDescriptors: [NSSortDescriptor]?
-    public let limit: Int?
-    public let offset: Int?
+    public var predicate: NSPredicate?
+    public var sortDescriptors: [NSSortDescriptor]?
+    public var limit: Int?
+    public var offset: Int?
+    
+    public init(predicate: NSPredicate?, sortDescriptors: [NSSortDescriptor]?, limit: Int?, offset: Int?) {
+        self.predicate = predicate
+        self.sortDescriptors = sortDescriptors
+        self.limit = limit
+        self.offset = offset
+    }
     
     public static func empty() -> FetchParameters {
         return FetchParameters(predicate: nil, sortDescriptors: nil, limit: nil, offset: nil)
